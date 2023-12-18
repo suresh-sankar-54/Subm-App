2edea9c3af4cfa4ec05c25221c7857869aabe8e1
suresh-sankar-54 <116792443+suresh-sankar-54@users.noreply.github.com>
2023-12-18 06:04:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,49 @@
+const Stack = createNativeStackNavigator();
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import HomePage from "./screens/HomePage";
+import Insights from "./screens/Insights";
+import ProfilePage from "./screens/ProfilePage";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
+
+  const [fontsLoaded, error] = useFonts({
+    "BwModelica-Regular": require("./assets/fonts/BwModelica-Regular.otf"),
+  });
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return (
+    <>
+      <NavigationContainer>
+        {hideSplashScreen ? (
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+              name="HomePage"
+              component={HomePage}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Insights"
+              component={Insights}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="ProfilePage"
+              component={ProfilePage}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        ) : null}
+      </NavigationContainer>
+    </>
+  );
+};
+export default App;
---
+++ GlobalStyles.js
@@ -0,0 +1,42 @@
+/* fonts */
+export const FontFamily = {
+  regular10: "BwModelica-Regular",
+};
+/* font sizes */
+export const FontSize = {
+  regular12_size: 12,
+  size_sm: 14,
+  regular10_size: 10,
+  medium11_size: 11,
+  bold16_size: 16,
+};
+/* Colors */
+export const Color = {
+  color: "#212121",
+  a1A1A: "#1a1a1a",
+  style: "#bfbfbf",
+  colorSilver_100: "#b5b5b5",
+  colorYellowgreen: "#bdf271",
+  color1: "#373737",
+  fFFFFF: "#fff",
+  colorLightpink_100: "#fcb0b4",
+  colorLightpink_200: "#fdadb2",
+  colorCornflowerblue: "#9c9cf0",
+  colorLightblue: "#bae0ff",
+  e8F99: "#7e8f99",
+};
+/* Paddings */
+export const Padding = {
+  p_21xl: 40,
+  p_xs: 12,
+  p_9xs: 4,
+  p_7xs: 6,
+  p_5xs: 8,
+};
+/* border radiuses */
+export const Border = {
+  br_5xs: 8,
+  br_9xs: 4,
+  br_11xs: 2,
+  br_xs: 12,
+};
